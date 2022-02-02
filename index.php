b1ecfe1403473fb702588526b855c65207fd96c3
SufianHamdan <sufian_hamdan@yahoo.com>
2022-02-02 11:51:05 -0800

    done

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+  echo "Q1 ------> "; 
+  $printHello =  "sufian ";
+
+  echo $printHello;
+?>
+
+<?php 
+  $var1 = "variable 1";
+  $var2 = "variable 2";
+  
+  echo "<br> <br> <br>";
+  echo "Q2 ------> ";
+  echo $var1." , ".$var2;
+?>
+
+<?php
+  echo "<br> <br> <br>";
+  echo "Q3: ";
+  echo "<h1>This is an h1 tag inside a php script</h1>";
+  echo "<p>This is a paragraph tag inside a php script</p>";
+?>
+</body>
+</html>
+
